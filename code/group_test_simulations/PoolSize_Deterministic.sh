99358e312e139f294dadbf51b6d060f60e5bf2de
David Hong <dahong67@wharton.upenn.edu>
2020-06-12 19:47:30 -0400

    Fix deterministic design script

--- code/group_test_simulations/PoolSize_Deterministic.sh
+++ code/group_test_simulations/PoolSize_Deterministic.sh
@@ -9,10 +9,10 @@ export OMP_NUM_THREADS=1
 
 BASE=../../../covid19-group-tests-data
 
-OUT1=Det1
-DESIGN1=Pooling\ \(higher\ prevalence,\ 96\ samples,\ 6\ pools\).npy
-python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT1/ --start-time 0 --end-time 120 --pool-compositions $BASE/$DESIGN1
+OUT1=Results_det1
+DESIGN1="Pooling (higher prevalence, 96 samples, 6 pools).npy"
+python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT1/ --start-time 0 --end-time 120 --pool-compositions "$BASE/$DESIGN1"
 
-OUT2=Det2
-DESIGN2=Pooling\ \(lower\ prevalence,\ 192\ samples,\ 12\ pools\).npy
-python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT2/ --start-time 0 --end-time 120 --pool-compositions $BASE/$DESIGN2
+OUT2=Results_det2
+DESIGN2="Pooling (lower prevalence, 192 samples, 12 pools).npy"
+python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT2/ --start-time 0 --end-time 120 --pool-compositions "$BASE/$DESIGN2"
