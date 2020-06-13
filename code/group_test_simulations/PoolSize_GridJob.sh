3cc0112c8ce222ed30ec785ece1766232bf6894d
David Hong <dahong67@wharton.upenn.edu>
2020-06-12 20:07:32 -0400

    Fix grid script

--- code/group_test_simulations/PoolSize_GridJob.sh
+++ code/group_test_simulations/PoolSize_GridJob.sh
@@ -12,4 +12,4 @@ BASE=../../../covid19-group-tests-data
 OUT=Results_grid
 read n1 n2 <<< $(sed -n ${SGE_TASK_ID}p grid_params.txt)
 
-python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT/ --start-time 0 --end-time 120 --grid-balanced '$n1,$n2'
+python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT/ --start-time 0 --end-time 120 --grid-balanced "$n1,$n2"
