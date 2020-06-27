88265c3d0e4c9d3d1b58e89bc0902e1eeb69b46e
David Hong <dahong67@wharton.upenn.edu>
2020-06-27 14:59:27 -0400

    New sweep of comb designs

--- code/group_test_simulations/PoolSize_CombJob.sh
+++ code/group_test_simulations/PoolSize_CombJob.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 #$ -N PoolSizeComb
+#$ -t 1-16
 #$ -l m_mem_free=20G
 
 sleep $((SGE_TASK_ID%60))
@@ -9,6 +10,6 @@ export OMP_NUM_THREADS=1
 
 BASE=../../../covid19-group-tests-data
 OUT=Results_comb
+read n m q <<< $(sed -n ${SGE_TASK_ID}p comb_params.txt)
 
-python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT/ --start-time 0 --end-time 120 --pool-compositions $BASE/Design_comb/Acomb.n-96_m-6_q-2.npy
-python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT/ --start-time 0 --end-time 120 --pool-compositions $BASE/Design_comb/Acomb.n-192_m-12_q-2.npy
+python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT/ --start-time 0 --end-time 120 --pool-compositions $BASE/Design_comb/Acomb.n-${n}_m-${m}_q-${q}.npy
