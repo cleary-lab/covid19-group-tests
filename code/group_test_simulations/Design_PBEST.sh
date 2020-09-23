08bf715df886bd4e8776347f383024a639024c64
David Hong <dahong67@wharton.upenn.edu>
2020-09-23 00:59:37 -0400

    Fix typo

--- code/group_test_simulations/Design_PBEST.sh
+++ code/group_test_simulations/Design_PBEST.sh
@@ -9,4 +9,4 @@ export OMP_NUM_THREADS=1
 BASE=../../../../2020.03.group-testing/covid19-group-tests-data
 OUT=Results_PBEST
 
-python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT1/ --start-time 0 --end-time 120 --pool-compositions $BASE/Design_PBEST/poolingMatrix.npy
+python -u test_on_simulated_population.py --viral-load-matrix $BASE/Simulated_populations/seir_viral_loads.identification.csv --infection-time $BASE/Simulated_populations/infection_times.csv --onset-time $BASE/Simulated_populations/onset_times.csv --expected-pos-prob 0.95 --savepath $BASE/$OUT/ --start-time 0 --end-time 120 --pool-compositions $BASE/Design_PBEST/poolingMatrix.npy
